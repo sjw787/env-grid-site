558d6144c8f367bb27eb4bfef5710fd95060c9cd
sjw787 <swylock@albany.edu>
2017-07-29 16:24:02 -0400

    can now add more cells with addcell button

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -1,6 +1,5 @@
 import './Grid.css'
 import React, { Component } from 'react';
-import GridHeader from './GridHeader'
 import GridCell from './GridCell'
 const items = [
   {
@@ -46,21 +45,58 @@ const items = [
     key: 6
   }
 ]
+
+
+
 class Grid extends Component{
   constructor(props){
     super(props)
-    console.log("props", props);
-    const cells = items.map((item, index) =>
-      <GridCell key={index} name={item.name} value={item.value} inc={item.inc} dec={item.dec}/>
+    let cells = items.map((item, index) =>
+      <GridCell key={index} name={item.name } value={item.value} inc={item.inc} dec={item.dec}/>
     )
     this.state = {body: cells}
+  }
+
+  mapCells(items){
+    return items.map((item, index) =>
+      <GridCell key={index} name={item.name } value={item.value} inc={item.inc} dec={item.dec}/>
+    )
+  }
+
+  addCell(e){
+    e.preventDefault();
+    this.setState(function(prevState, props){
+      let newBody = items;
+      console.log("newBody",newBody);
+      newBody.push({
+          name: "newItem",
+          value: 5,
+          inc: 1,
+          dec: 1,
+          key: 6
+      });
+
+      return {
+        body: this.mapCells(newBody)
+      };
+    });
   }
 
   render(){
     console.log((this.state.body));
     return (
       <div id="Grid-container">
-        <GridHeader/>
+        <div>
+          <div id="Grid-controls">
+            <button id="Grid-add-button" onClick={this.addCell.bind(this)}>Click To Add a Story</button>
+          </div>
+          <div id="GridHeader-container">
+            <div className="GridHeader-item GH-item-border-right">Queued</div>
+            <div className="GridHeader-item GH-item-border-right GH-item-border-left">QA-1</div>
+            <div className="GridHeader-item GH-item-border-right GH-item-border-left">QA-2</div>
+            <div className="GridHeader-item GH-item-border-left">QA-3</div>
+          </div>
+        </div>
         <div id="Grid-content">
           {this.state.body}
         </div>
